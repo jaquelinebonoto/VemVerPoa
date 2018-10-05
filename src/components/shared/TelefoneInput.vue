180d5ca7d95d909a715e94215a9972c239b55f08
Jaqueline <jaqueline.bonoto@dbccompany.com.br>
2018-10-05 13:00:27 -0300

    feat(Esqueleto Arquivos): Inserção de elementos na pasta screen e shared

---
+++ src/components/shared/TelefoneInput.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="form-group">
+    <input type="text" v-validate="'required|email|isDBC'" :name="email" v-model="valor" class="form-control" :id="email" aria-describedby="emailHelp" placeholder="usuario@email.com">
+    <small id="emailHelp" class="form-text text-muted">Não vamos compartilhar seu email com ninguém.</small>
+    <span v-show="errors.has(this.email)">{{ errors.first(this.email) }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "EmailInput",
+  props: {
+    email: String
+  },
+  data: () => {
+    return {
+      valor: ""
+    };
+  },
+  computed: {}
+};
+</script>
